78d8cca8599ba3ad953dd4e43b7ca69356d3872e
ManuelDs28 <ktireds2803@gmail.com>
2023-04-17 20:55:13 +0200

    feat: modal

---
+++ src/components/EditCommentModal/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { CommentForm } from "../CommentForm";
+import "./index.css";
+
+export const EditCommentModal = ({ contentModal, setShowModal }) => {
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div className="edit-comment">
+      <div className="modal-content">
+        <div className="modal-header">
+        <h2>Edit Comment</h2>
+          <span onClick={() => closeModal()} className="close">
+            &times;
+          </span>
+        </div>
+        <div className="modal-body">
+          <CommentForm contentComment={contentModal} closeModal={closeModal}/>
+        </div>
+      </div>
+    </div>
+  );
+};
