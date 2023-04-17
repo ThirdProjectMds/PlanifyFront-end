7db81fba29798bcdfcbea90b290fb60957c76b97
ManuelDs28 <ktireds2803@gmail.com>
2023-04-17 20:56:52 +0200

    commet list with buttons

--- src/components/CommentList/index.jsx
+++ src/components/CommentList/index.jsx
@@ -1,22 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { deleteCommment, editComment } from "../../services/CommentService";
 import "./index.css";
+import { EditCommentModal } from "../EditCommentModal";
+
 export const CommentList = ({ comments }) => {
- 
+  const [showModal, setShowModal] = useState(false);
+  const  [contentComment, setContentComment ] = useState({})
+
+  
+  const handleEditComment = (postId,commentId, commentContent) => {
+    setContentComment({postId, commentId, commentContent })
+    setShowModal(true);
+  };
+
   return (
     <div className="comment-list">
       {!comments
         ? ""
         : comments.map((comment) => {
             return (
-              <div key={comment._id}>
-              <span>{comment.author.firstName}</span>
+              <div className="card-list" key={comment._id}>
+                <div className="info-user">
+                  <img src={comment.author.avatar} />
+                  <span>{comment.author.firstName}</span>
+                </div>
                 <li>
                   <p>{comment.content}</p>
                 </li>
+                <div className="btn">
+                  <button onClick={() => handleEditComment(comment. postId,comment._id, comment.content)}>
+                    🖊️
+                  </button>
+
+                  <button onClick={() => deleteCommment(comment._id)}>
+                    🗑️
+                  </button>
+                </div>
               </div>
-            )
-          })
-        }
+            );
+          })}
+      {showModal && <EditCommentModal contentModal={contentComment} setShowModal={setShowModal}/>}
     </div>
   );
 };
-
