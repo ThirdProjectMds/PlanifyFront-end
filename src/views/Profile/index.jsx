da7f298c7c0a7ee671ca241eeeb61e6517bb5b7f
ManuelDs28 <ktireds2803@gmail.com>
2023-04-19 03:55:18 +0200

    feat : profile with posts

--- src/views/Profile/index.jsx
+++ src/views/Profile/index.jsx
@@ -1,12 +1,34 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import AuthContext from "../../contexts/AuthContext";
-
+import { myPost } from "../../services/PostService";
+import { CardPost } from "../../components/CardPost";
+import "./index.css"
 export const Profile = () => {
-  const { currentUser } = useContext(AuthContext)
+  const { currentUser } = useContext(AuthContext);
+  const [myPosts, setMyPosts] = useState([]);
+
+  useEffect(() => {
+    myPost()
+      .then((myPostsResponse) => {
+        setMyPosts(myPostsResponse);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="profile">
-    <h1>Profile</h1>
-    <h3>{currentUser.firstName}</h3>
+      <h1>Profile</h1>
+      <h3>{currentUser.firstName}</h3>
+
+      {myPosts.map((post) => (
+        <CardPost data={post} key={post.id} />
+      ))}
     </div>
-  )
-}
+  );
+};
+
+
+
+
+
+
