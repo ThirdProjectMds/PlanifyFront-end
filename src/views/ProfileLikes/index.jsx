ce4319be2e955693b7e148be37a08a2bb0a588c0
ManuelDs28 <ktireds2803@gmail.com>
2023-04-21 02:27:17 +0200

    feat: profile likes

--- src/views/ProfileLikes/index.jsx
+++ src/views/ProfileLikes/index.jsx
@@ -1,25 +1,33 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { myLiked } from '../../services/LikeService'
 import AuthContext from '../../contexts/AuthContext'
+import { CardPost } from '../../components/CardPost'
+import "./index.css"
 
 export const ProfileLikes = () => {
-  const {currentUser} = useContext(AuthContext)
-  const [myLikedPosts, setMyLikedPosts] = useState({})
+  const { currentUser } = useContext(AuthContext)
+  const [myLikedPosts, setMyLikedPosts] = useState([])
 
-  useEffect(()=> {
-  myLiked()
-  .then(((myLikedPostsResponse)=> {
-    setMyLikedPosts(myLikedPostsResponse)
-  }))
-  .catch((err) => console.log(err));
-
+  useEffect(() => {
+    myLiked()
+      .then((myLikedPostsResponse) => {
+        setMyLikedPosts(myLikedPostsResponse)
+        console.log(myLikedPostsResponse);
+      })
+      .catch((err) => console.log(err));
   }, [])
-
+  console.log(myLikedPosts);
   return (
     <div className='profile-likes'>
-    <h1>Profile</h1>
-    <h3>{currentUser.firstName}</h3>
-
+      <h1>Profile</h1>
+      <h3>{currentUser.firstName}</h3>
+      {myLikedPosts.map((post) => (
+        <CardPost 
+        pageLike={true}
+        data={post.postId} 
+        key={post._id} 
+      />
+      ))}
     </div>
   )
 }
