d83f4af136d778f681570fbff089f46526c9181a
ManuelDs28 <ktireds2803@gmail.com>
2023-04-22 18:10:13 +0200

    feat: footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import  "./index.css"
+export const Footer = () => {
+  return (
+    <div className='Footer'>
+    <footer className="">
+    <div className=" up-footer  p-4 ">
+      <section className="">
+     
+      </section>
+    </div>
+  
+    <div className="text-center text-white p-3 down-footer">
+      © 2023 Planify:
+      <a id='text-a'  href={"/posts"}>All places</a>
+    </div>
+  </footer>
+    </div>
+  )
+}
