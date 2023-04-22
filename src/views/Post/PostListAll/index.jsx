0ed506c8013de683800da78f0c0703ed9e1af567
ManuelDs28 <ktireds2803@gmail.com>
2023-04-22 03:02:05 +0200

    feat: button more posts

--- src/views/Post/PostListAll/index.jsx
+++ src/views/Post/PostListAll/index.jsx
@@ -1,45 +1,73 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react';
 import { getPosts } from '../../../services/PostService';
 import { CardPost } from '../../../components/CardPost';
 import "./index.css"
 
 export const PostListAll = () => {
   const [posts, setPosts] = useState([]);
+  const [displayedPosts, setDisplayedPosts] = useState([]);
   const [selectedButton, setSelectedButton] = useState('');
-  
+  const [noMorePosts, setNoMorePosts] = useState(true)
   useEffect(() => {
     getPosts()
     .then((posts) => {
       setPosts(posts);
+      setDisplayedPosts(posts.slice(0, 5));
     })
     .catch((err) => console.log(err));
   }, []);
 
   const handleDeletePost = (id) => {
     setPosts(posts.filter((post) => post.id !== id));
+    setDisplayedPosts(displayedPosts.filter((post) => post.id !== id));
   };
+
+  const addRandomPosts = useCallback(() => {
+    if (posts.length > displayedPosts.length) {
+      const remainingPosts = posts.filter((post) => !displayedPosts.includes(post));
+      const randomIndices = new Set();
+      while (randomIndices.size < 5) {
+        randomIndices.add(Math.floor(Math.random() * remainingPosts.length));
+      }
+      const randomPosts = Array.from(randomIndices).map((index) => remainingPosts[index]);
+      setDisplayedPosts([...displayedPosts, ...randomPosts]);
+      setSelectedButton('more');
+    }else{
+      setNoMorePosts(false)
+    }
+  }, [posts, displayedPosts]);
+
   const postsSortByLikes = useCallback(() => {
-    setPosts([...posts].sort((a, b) => b.likes.length - a.likes.length));
+    setDisplayedPosts([...displayedPosts].sort((a, b) => b.likes.length - a.likes.length));
     setSelectedButton('likes');
-  }, [posts]);
+  }, [displayedPosts]);
+
   const postsSortByLatest = useCallback(() => {
-    setPosts([...posts].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)));
+    setDisplayedPosts([...displayedPosts].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)));
     setSelectedButton('latest');
-  }, [posts]);
+  }, [displayedPosts]);
+
   const postsSortByOldest = useCallback(() => {
-    setPosts([...posts].sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt)));
+    setDisplayedPosts([...displayedPosts].sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt)));
     setSelectedButton('oldest');
-  }, [posts]);
+  }, [displayedPosts]);
 
   return (
     <div className='post-list-all'>
-    <div className='sort-buttons'>
-    <button onClick={postsSortByLikes} className={selectedButton === 'likes' ? 'selected' : ''}>Most Liked</button>
-    <button onClick={postsSortByLatest}  className={selectedButton === 'latest' ? 'selected' : ''}>Latest Posts</button>
-    <button onClick={postsSortByOldest}className={selectedButton === 'oldest' ? 'selected' : ''} >Oldest Post</button>
+      <div className='sort-buttons'>
+        <button onClick={postsSortByLikes} className={selectedButton === 'likes' ? 'selected' : ''}>Most Liked</button>
+        <button onClick={postsSortByLatest}  className={selectedButton === 'latest' ? 'selected' : ''}>Latest Posts</button>
+        <button onClick={postsSortByOldest} className={selectedButton === 'oldest' ? 'selected' : ''} >Oldest Post</button>
+        </div>
+        
+        {displayedPosts.map((post) => <CardPost data={post} key={post.id} onDeletePost={handleDeletePost} />)}
+       {noMorePosts && 
+        <div className='add-more' > 
+       
+       <button  onClick={addRandomPosts} className={selectedButton === 'more' ? 'selected' : ''}>More posts</button>
+       </div>
+      
+      }
     </div>
-    {posts.map((post) => <CardPost data={post} key={post.id} onDeletePost={handleDeletePost} />)}
-    </div>
-    )
-  }
-  
+  );
+}
